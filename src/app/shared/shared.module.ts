380ad5a9f3c788cfa3bfdaf3ea52d4a16ceced49
SebastianBarciaBeltran <sebastianbarciabeltran@gmail.com>
2023-01-21 13:32:14 +0100

    structure of the project implementd

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SharedRoutingModule } from './shared-routing.module';
+import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HeaderComponent } from './components/header/header.component';
+
+@NgModule({
+  declarations: [
+    BreadcrumbsComponent,
+    SidebarComponent,
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedRoutingModule
+  ]
+})
+export class SharedModule { }
